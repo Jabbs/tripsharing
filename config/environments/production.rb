d982c77d7ad52d60262f159701635d6a7781e015
Peter Jabbour <petejabbour1@gmail.com>
2014-01-29 09:40:54 -0600

    removed compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Tripsharing::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
