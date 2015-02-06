f4eec0ae11d1f61641a7853ee15ece7507f671e4
Pete Jabbour <pete@learnmetrics.com>
2015-02-06 07:40:00 -0600

    lots

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   
   def home
-    @fb_images_rand_5 = User.fb_image_random_5
+    # @fb_images_rand_5 = User.fb_image_random_5
     # @pete = User.find_by_email("petejabbour1@gmail.com")
     # @pete_rand5 = @pete.facebook_friends_photos_rand5
     @session_user = User.new
