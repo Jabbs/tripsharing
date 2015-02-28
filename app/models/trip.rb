3dcf8fb260314e14f94b4bae78e916dac4dff6de
Pete Jabbour <pete@learnmetrics.com>
2015-02-28 16:59:53 -0600

    images

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -23,10 +23,16 @@ class Trip < ActiveRecord::Base
                      ["16+", "8"], ["tbd", "9"]]
   GROUP_COUNT = {"1" => "1", "2" => "2", "3" => "3", "4" => "4", "5" => "5", "6" => "6-10", "7" => "11-15", "8" => "16+", "9" => "tbd"}
   CURRENCIES = ["AUD","CAD","CHF","CNY","EUR","GBP","HKD","IDR","INR","JPY","MXN","NZD","RUB","SEK","SGD","THB","USD","ZAR"]
-  REGIONS = {"1" => "Europe", "2" => "Africa", "3" => "East Asia and the Pacific", "4" => "South Asia", "5" => "Middle East", "6" => "N. America",
-             "7" => "S. America", "8" => "Central America"}
-  REGIONS_ARRAY = [["Europe", "1"], ["Africa", "2"], ["East Asia and the Pacific", "3"], ["South Asia", "4"], ["Middle East", "5"], ["N. America", "6"],
-             ["S. America", "7"], ["Central America", "8"]]
+  
+  
+  REGIONS = {"1" => "Europe", "2" => "Africa", "3" => "East Asia", "4" => "South Asia", "5" => "Southeast Asia", "6" => "N. Asia", "7" => "N. America",
+             "8" => "S. America", "9" => "Central America", "10" => "Australia and South Pacific", "11" => "Middle East", "12" => "Russia, Central Asia, and Transcaucasia",
+             "13" => "Antarctica"}
+  REGIONS_ARRAY = [["Europe", "1"], ["Africa", "2"], ["East Asia", "3"], ["South Asia", "4"], ["Southeast Asia", "5"], ["N. Asia", "6"], ["N. America", "7"],
+             ["S. America", "8"], ["Central America", "9"], ["Australia and South Pacific", "10"], ["Middle East", "11"], ["Russia, Central Asia, and Transcaucasia", "12"],
+             ["Antarctica", "13"]]
+  
+  
   DURATIONS = [["(1-3 days) quick", "1"], ["(4-14 days) short", "2"], ["(15-24 days) tolerable", "3"], ["(25-44 days) long", "4"], ["(45+ days) extended", "5"], ["unknown", "6"]]
   DEPARTINGS_ARRAY = [["today", "1"], ["asap", "2"], ["this weekend", "3"], ["spring 2015", "4"], ["summer 2015", "5"], ["fall 2015", "6"], ["winter 2015", "7"],
                ["spring 2016", "8"], ["summer 2016", "9"], ["fall 2016", "10"], ["winter 2016", "11"]]
