9065f2835dfee42569e1975ef612d926b3773aff
Pete Jabbour <pete@learnmetrics.com>
2015-03-23 08:15:47 -0500

    changed popup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,5 @@ OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
            scope: 'email, user_birthday, user_hometown, user_location,
-           user_groups, user_interests, user_likes, user_photos, friends_photos', 
-           display: 'popup'
+           user_groups, user_interests, user_likes, user_photos, friends_photos'
 end
