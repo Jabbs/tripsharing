974cb7d0914d9d223d6bdb02a592fe9bdf7a8a79
Pete Jabbour <pete@learnmetrics.com>
2015-03-23 08:53:06 -0500

    more assets

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Without this change, the application would serve the precompiled assets 
   # from /assets in development, and you would not see any local changes until you compile assets again.
   # http://guides.rubyonrails.org/v4.2.0/asset_pipeline.html#precompiling-assets
-  config.assets.prefix = "/dev-assets"
+  # config.assets.prefix = "/dev-assets"
   
   # Send out mailers when .delivery is invoked
   config.action_mailer.perform_deliveries = true
