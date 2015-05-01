0bd7d4685cdbabe8be875902f5937d7f9a183175
Pete Jabbour <pete@learnmetrics.com>
2015-05-01 12:45:34 -0500

    removed more fb stuff

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,8 +2,9 @@ OmniAuth.config.logger = Rails.logger
 
 # removed "user_groups", "user_photos" on 4/9/15
 # removed "user_interests" on 4/9/15 bc it is depreciating in June (was a subset of user_likes)
+# removed "user_tagged_places" on 5/1/15
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
            scope: 'email, user_birthday, user_hometown, user_location,
-           friends_photos, user_tagged_places'
+           friends_photos'
 end
