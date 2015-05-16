f069d7c53524b26f029650ee24e3ef928fad239a
Pete Jabbour <pete@learnmetrics.com>
2015-05-16 09:12:55 -0500

    region highlight hack

---
+++ app/workers/newsletter_worker.rb
@@ -0,0 +1,17 @@
+class NewsletterWorker
+  include Sidekiq::Worker
+  sidekiq_options backtrace: true, retry: 3, failures: :exhausted
+
+  def perform(user_id, trip_ids)
+    user = User.find_by_id(user_id)
+    user.generate_token(:verification_token)
+    user.save
+    unless user.verification_sent_at && user.verification_sent_at > 10.minute.ago
+      UserMailer.user_verification_email(user).deliver
+    end
+  end
+  
+  sidekiq_retries_exhausted do |msg|
+    Sidekiq.logger.warn "Failed #{msg['class']} with #{msg['args']}: #{msg['error_message']}"
+  end
+end
